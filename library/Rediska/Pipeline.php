770b654c4fbe418c2d4d3f37efeb074a58ae1cc9
ivanshumkov <ivanshumkov@d17a2f14-6c28-47c4-8f53-41d4632e5d06>
2010-01-13 09:04:05 +0000

    Rmove trash
    git-svn-id: https://svn.geometria-lab.net/rediska/trunk@82 d17a2f14-6c28-47c4-8f53-41d4632e5d06

--- library/Rediska/Pipeline.php
+++ library/Rediska/Pipeline.php
@@ -9,26 +9,67 @@ class Rediska_Pipeline
      */
     protected $_rediska;
 
-    public function __construct(Rediska $rediska)
+    /**
+     * Rediska specified connection instance
+     * 
+     * @var Rediska_Connection_Specified
+     */
+    protected $_specifiedConnection;
+
+    /**
+     * Default pipeline connection
+     * 
+     * @var Rediska_Connection
+     */
+    protected $_defaultConnection;
+
+    /**
+     * Commands buffer
+     * 
+     * @var array
+     */
+    protected $_commands = array();
+
+    public function __construct(Rediska $rediska, Rediska_Connection_Specified $specifiedConnection)
     {
         $this->_rediska = $rediska;
+        $this->_specifiedConnection = $specifiedConnection;
+        $this->_defaultConnection = $specifiedConnection->getConnection();
     }
 
     public function execute()
     {
-        
-        
-        
-        return $responses;
+        if (empty($this->_commands)) {
+            throw new Rediska_Exception("Nothing to execute!");
+        }
+
+        foreach($this->_commands as $command) {
+            $command->write();
+        }
+
+        $results = array();
+        foreach($this->_commands as $command) {
+            $results[] = $command->read();
+        }
+
+        return $results;
     }
 
     public function __call($name, $args)
     {
-        
-    }
+        if (strtolower($name) == 'on' && isset($args[0])) {
+            $this->_rediska->on($args[0]);
+            $connection = $this->_specifiedConnection->getConnection();
+        } else {
+            $connection = $this->_defaultConnection;
+        }
 
-    public function __toString()
-    {
-        
+        $this->_specifiedConnection->setConnection($connection);
+ 
+        $this->_commands[] = $this->_rediska->getCommand($name, $args);
+
+        $this->_specifiedConnection->resetConnection();
+
+        return $this;
     }
 }
