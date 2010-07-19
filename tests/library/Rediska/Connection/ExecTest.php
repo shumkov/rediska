d39d9af92c2b81e27727abf901f1faed2e8fc17f
Ivan Shumkov <ivan@shumkov.ru>
2010-07-19 19:25:26 +0400

    add test

---
+++ tests/library/Rediska/Connection/ExecTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class Rediska_Connection_ExecTest extends Rediska_TestCase
+{
+    public function testWrite()
+    {
+        $this->markTestIncomplete('Write me!');
+    }
+
+    public function testRead()
+    {
+        $this->markTestIncomplete('Write me!');
+    }
+
+    public function testExecute()
+    {
+        $this->markTestIncomplete('Write me!');
+    }
+
+    public function testTransformMultiBulkCommand()
+    {
+        $this->markTestIncomplete('Write me!');
+    }
+
+    public function testReadResponseFromConnection()
+    {
+        $this->markTestIncomplete('Write me!');
+    }
+}
