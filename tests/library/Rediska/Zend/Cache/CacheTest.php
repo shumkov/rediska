023b0a4d2d492a38a2d781b0d178b3fa9ac88c8e
Ivan Shumkov <ivan@shumkov.ru>
2010-08-25 13:28:48 +0400

    fix cache backend options

--- tests/library/Rediska/Zend/Cache/CacheTest.php
+++ tests/library/Rediska/Zend/Cache/CacheTest.php
@@ -15,7 +15,7 @@ class Rediska_Zend_Cache_BackendTest extends Rediska_TestCase
 
         Rediska_Autoloader::load('Rediska_Zend_Cache_Backend_Redis');
 
-        $this->cache = Zend_Cache::factory('Core', 'Rediska_Zend_Cache_Backend_Redis', array(), Rediska::DEFAULT_NAME, false, true, true);
+        $this->cache = Zend_Cache::factory('Core', 'Rediska_Zend_Cache_Backend_Redis', array(), array(), false, true, true);
     }
     
     public function testLoad()
