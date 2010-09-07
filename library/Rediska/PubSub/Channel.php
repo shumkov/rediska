03833c8c97a1ecf9306148e93e54a0348ecdaf91
Ivan Shumkov <ivan@shumkov.ru>
2010-09-07 16:02:26 +0400

    add PubSub subcategory

--- library/Rediska/PubSub/Channel.php
+++ library/Rediska/PubSub/Channel.php
@@ -8,6 +8,7 @@ require_once dirname(__FILE__) . '/../../../Rediska.php';
  *
  * @author Ivan Shumkov
  * @package Rediska
+ * @subpackage PublishSubscribe
  * @version @package_version@
  * @link http://rediska.geometria-lab.net
  * @license http://www.opensource.org/licenses/bsd-license.php
